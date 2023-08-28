260c789139c214001c136cad8ba913d62886d13c
Mehakb7 <b7mehak@gmail.com>
2023-08-28 21:03:31 +0530

    use access token for whoami

--- client/src/services/whoami.js
+++ client/src/services/whoami.js
@@ -5,20 +5,10 @@ export const whoAmi = BaseApi.injectEndpoints({
   endpoints: (builder) => ({
     getWhoAmI: builder.query({
       query: () => ({
-        url: `${END_POINTS.whoami}?email=${localStorage.getItem("email")}`,
-        method: "GET",
-      }),
-      transformResponse: (res, meta, args) => {
-        return res;
-      },
-    }),
-
-    refreshToken: builder.query({
-      query: () => ({
         url: `${END_POINTS.whoami}`,
         method: "GET",
         headers: {
-          authorization: localStorage.getItem("refresh"),
+          authorization: localStorage.getItem("token"),
         },
       }),
       transformResponse: (res, meta, args) => {
@@ -28,5 +18,5 @@ export const whoAmi = BaseApi.injectEndpoints({
   }),
 });
 
-export const { useGetWhoAmIQuery, useLazyRefreshTokenQuery } = whoAmi;
+export const { useGetWhoAmIQuery } = whoAmi;
 export const userInfo = whoAmi.endpoints.getWhoAmI.select();
